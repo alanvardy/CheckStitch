b136ade9d218954a4e83899a5c8d94156cba114b
Alan Vardy <alanvardy@gmail.com>
2026-09-09 13:11:22 -0700

    Add centered checklist button that creates three reminders

--- CheckStitch/ContentView.swift
+++ CheckStitch/ContentView.swift
@@ -1,9 +1,28 @@
+import EventKit
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("checklist") {
+            Task { await createChecklistReminders() }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+    }
+
+    func createChecklistReminders() async {
+        let eventStore = EKEventStore()
+        do {
+            let granted = try await eventStore.requestFullAccessToReminders()
+            if !granted { return }
+            for title in ["one", "two", "three"] {
+                let reminder = EKReminder(eventStore: eventStore)
+                reminder.title = title
+                reminder.calendar = eventStore.defaultCalendarForNewReminders()
+                try eventStore.save(reminder, commit: true)
+            }
+        } catch {
+            return
+        }
     }
 }
 
